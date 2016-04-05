0ce7d39c9efd0dcc4e8e62751245af8b9a8ab5b4
Marito <mariobrassescogarcia@gmail.com>
2016-04-05 17:48:57 +0200

    Finished last iteration. Trying to refactor it by creating a class Calculator in lib folder to avoid running logic on server.rb

--- server.rb
+++ server.rb
@@ -1,8 +1,46 @@
 require "sinatra"
 require "sinatra/reloader"
 require "pry"
+# require "lib/calculator"
 
 
-get "/add" do
-	erb(:add)	
+get "/" do
+	erb(:home)	
 end
+
+
+
+post "/calculate_add" do
+	"Params data:" + params.inspect
+	first = params[:first_number].to_f
+	second = params[:second_number].to_f
+	result = first + second
+	"#{first} + #{second} = #{result}"
+end
+
+
+post "/calculate_substraction" do
+	"Params data:" + params.inspect
+	first = params[:first_number].to_f
+	second = params[:second_number].to_f
+	result = first - second
+	"#{first} - #{second} = #{result}"
+end
+
+post "/calculate_multiplication" do
+	"Params data:" + params.inspect
+	first = params[:first_number].to_f
+	second = params[:second_number].to_f
+	result = first * second
+	"#{first} x #{second} = #{result}"
+end
+
+post "/calculate_division" do
+	"Params data:" + params.inspect
+	first = params[:first_number].to_f
+	second = params[:second_number].to_f
+	result = first / second
+	"#{first} / #{second} = #{result}"
+end
+
+
