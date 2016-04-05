bf76ee40441384faf3143d86426148e78328c43c
Marito <mariobrassescogarcia@gmail.com>
2016-04-05 18:56:27 +0200

    Merged all inputs into only one form

--- server.rb
+++ server.rb
@@ -10,25 +10,43 @@ end
 
 
 
-post "/calculate_add" do
-	"Params data:" + params.inspect
-	" #{params[:first_number].to_f} + #{params[:second_number].to_f} = #{Calculator.new.add(params[:first_number].to_f, params[:second_number].to_f)}"
-end
+# post "/calculate_add" do
+# 	"Params data:" + params.inspect
+# 	" #{params[:first_number].to_f} + #{params[:second_number].to_f} = #{Calculator.new.add(params[:first_number].to_f, params[:second_number].to_f)}"
+# end
 
 
-post "/calculate_substraction" do
-	"Params data:" + params.inspect
-	" #{params[:first_number].to_f} - #{params[:second_number].to_f} = #{Calculator.new.substract(params[:first_number].to_f, params[:second_number].to_f)}"
-end
+# post "/calculate_substraction" do
+# 	"Params data:" + params.inspect
+# 	" #{params[:first_number].to_f} - #{params[:second_number].to_f} = #{Calculator.new.substract(params[:first_number].to_f, params[:second_number].to_f)}"
+# end
 
-post "/calculate_multiplication" do
-	"Params data:" + params.inspect
-	" #{params[:first_number].to_f} x #{params[:second_number].to_f} = #{Calculator.new.multiply(params[:first_number].to_f, params[:second_number].to_f)}"
-end
+# post "/calculate_multiplication" do
+# 	"Params data:" + params.inspect
+# 	" #{params[:first_number].to_f} x #{params[:second_number].to_f} = #{Calculator.new.multiply(params[:first_number].to_f, params[:second_number].to_f)}"
+# end
 
-post "/calculate_division" do
-	"Params data:" + params.inspect
-	" #{params[:first_number].to_f} / #{params[:second_number].to_f} = #{Calculator.new.divide(params[:first_number].to_f, params[:second_number].to_f)}"
-end
+# post "/calculate_division" do
+# 	"Params data:" + params.inspect
+# 	" #{params[:first_number].to_f} / #{params[:second_number].to_f} = #{Calculator.new.divide(params[:first_number].to_f, params[:second_number].to_f)}"
+# end
+
+
+
+ post "/calculate" do
+ 	"Params data:" + params.inspect
+ 	@operation = params[:operation] 
+ 	case @operation
+ 	when "addition"
+ 		" #{params[:first_number].to_f} + #{params[:second_number].to_f} = #{Calculator.new.add(params[:first_number].to_f, params[:second_number].to_f)}"		
+	when "substraction"
+		" #{params[:first_number].to_f} - #{params[:second_number].to_f} = #{Calculator.new.substract(params[:first_number].to_f, params[:second_number].to_f)}"
+	when "multiplication"
+		" #{params[:first_number].to_f} x #{params[:second_number].to_f} = #{Calculator.new.multiply(params[:first_number].to_f, params[:second_number].to_f)}"
+ 	else
+		" #{params[:first_number].to_f} / #{params[:second_number].to_f} = #{Calculator.new.divide(params[:first_number].to_f, params[:second_number].to_f)}"
+ 	end
+ 	
+ end
 
 
